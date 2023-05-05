ef60c138170e94ad2ef615b7e99c9e4368c85431
ngocsnake <ducngoc.dev@gmail.com>
2023-05-05 20:19:56 +0700

    Update code

---
+++ BE/src/test/java/vn/fs/ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.fs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
